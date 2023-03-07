20e492387207a39f05b2b0985a3ae6731dd11685
Aleksandrs Jansons <43913143+alexjansons@users.noreply.github.com>
2023-03-07 21:37:06 +0200

    Added successful disconnect case with missing error handler

--- web-socket/disconnect-error/src/main/java/com/example/websocket/disconnecterror/Application.java
+++ web-socket/disconnect-error/src/main/java/com/example/websocket/disconnecterror/Application.java
@@ -1,26 +1,25 @@
 package com.example.websocket.disconnecterror;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
-import org.springframework.stereotype.Component;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
 
-import java.util.concurrent.CountDownLatch;
-
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	public StompSubProtocolErrorHandler errorHandler() {
+		return new StompSubProtocolErrorHandler();
+	}
 
 	@Configuration
 	@EnableWebSocketMessageBroker
@@ -28,10 +27,15 @@ public class Application {
 		@Autowired
 		private StompSubProtocolErrorHandler errorHandler;
 
+		@Value("${errorHandler.enabled}")
+		private Boolean errorHandlerEnabled;
+
 		@Override
 		public void registerStompEndpoints(StompEndpointRegistry registry) {
-			registry.setErrorHandler(errorHandler)
-					.addEndpoint("/web-socket");
+			registry.addEndpoint("/web-socket");
+			if (errorHandlerEnabled) {
+				registry.setErrorHandler(errorHandler);
+			}
 		}
 
 		@Override
@@ -41,35 +45,6 @@ public class Application {
 					.setHeartbeatValue(new long[]{10000, 1000})
 					.setTaskScheduler(new DefaultManagedTaskScheduler());
 		}
-	}
-
-	@Component
-	static class WebSocketErrorHandler extends StompSubProtocolErrorHandler {
-		@Override
-		public Message<byte[]> handleErrorMessageToClient(Message<byte[]> errorMessage) {
-			try {
-				Message<byte[]> message = handleErrorMessageToClientFixed(errorMessage);
-				return message;
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw e;
-			}
-		}
-
-		public Message<byte[]> handleErrorMessageToClientFixed(Message<byte[]> errorMessage) {
-			StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(errorMessage, StompHeaderAccessor.class);
-			if (accessor == null || !accessor.isMutable()) {
-				accessor = StompHeaderAccessor.wrap(errorMessage);
-				accessor.addNativeHeader("sessionAction", "DISCONNECT");
-				StompCommand stompCommand = accessor.getCommand();
-				if (stompCommand == null) {
-//					accessor.setHeader("stompCommand", StompCommand.ERROR);
-				}
-			}
-			return handleInternal(accessor, errorMessage.getPayload(), null, null);
-		}
-
-
 	}
 
 	public static void main(String[] args) {
--- web-socket/disconnect-error/src/test/java/com/example/websocket/disconnecterror/ApplicationTests.java
+++ web-socket/disconnect-error/src/test/java/com/example/websocket/disconnecterror/ApplicationTests.java
@@ -1,15 +1,16 @@
 package com.example.websocket.disconnecterror;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.ConnectionLostException;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.Trigger;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
@@ -20,90 +21,85 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class ApplicationTests {
-	@LocalServerPort
-	private Integer port;
-	@Autowired
-	private Application.WebSocketErrorHandler errorHandler;
-
 	static class NoOpTaskScheduler implements TaskScheduler {
-
 		@Override
 		public ScheduledFuture<?> schedule(Runnable task, Trigger trigger) {
 			return null;
 		}
-
 		@Override
 		public ScheduledFuture<?> schedule(Runnable task, Instant startTime) {
 			return null;
 		}
-
 		@Override
 		public ScheduledFuture<?> scheduleAtFixedRate(Runnable task, Instant startTime, Duration period) {
 			return null;
 		}
-
 		@Override
 		public ScheduledFuture<?> scheduleAtFixedRate(Runnable task, Duration period) {
 			return null;
 		}
-
 		@Override
 		public ScheduledFuture<?> scheduleWithFixedDelay(Runnable task, Instant startTime, Duration delay) {
 			return null;
 		}
-
 		@Override
 		public ScheduledFuture<?> scheduleWithFixedDelay(Runnable task, Duration delay) {
 			return null;
 		}
 	}
 
-
-	@Test
-	void disconnectByHeartBeatTask() throws Exception {
-		String endpointUrl = String.format("ws://localhost:%d/web-socket", port);
-
-		WebSocketStompClient webSocketClient = new WebSocketStompClient(new StandardWebSocketClient());
-		CountDownLatch latch = new CountDownLatch(1);
+	void testDisconnectByHeartBeatTask(int port) throws Exception {
+		CountDownLatch countDownLatch = new CountDownLatch(1);
 		AtomicBoolean disconnectReceived = new AtomicBoolean(false);
+		String endpointUrl = String.format("ws://localhost:%d/web-socket", port);
+		WebSocketStompClient webSocketClient = new WebSocketStompClient(new StandardWebSocketClient());
 		StompSessionHandlerAdapter dummySessionHandler = new StompSessionHandlerAdapter() {
-			@Override
-			public void handleFrame(StompHeaders headers, Object payload) {
-				System.out.println("headers: " + headers);
-				System.out.println("payload: " + payload);
-				super.handleFrame(headers, payload);
-				if (headers.containsKey("sessionAction")) {
-					latch.countDown();
-					disconnectReceived.set(true);
-				}
-			}
-
-			@Override
-			public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
-				super.handleException(session, command, headers, payload, exception);
-			}
-
 			@Override
 			public void handleTransportError(StompSession session, Throwable exception) {
 				super.handleTransportError(session, exception);
-				//???
-				System.out.println("session: " + session);
+				assertTrue(exception instanceof ConnectionLostException);
 				exception.printStackTrace();
+				countDownLatch.countDown();
 				disconnectReceived.set(true);
 			}
 		};
 		webSocketClient.setDefaultHeartbeat(new long[]{1000, 10000});
-		// Emulate the case when the client does not send heart beats
 		webSocketClient.setTaskScheduler(new NoOpTaskScheduler());
-
 		StompSession session = webSocketClient.connectAsync(endpointUrl, dummySessionHandler).get();
 		session.subscribe("/topic", dummySessionHandler);
-		latch.await(10, TimeUnit.SECONDS);
+		countDownLatch.await(10, TimeUnit.SECONDS);
 		assertTrue(disconnectReceived.get());
+	}
+
+	@Nested
+	@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+	@DirtiesContext
+	@TestPropertySource(properties = {"errorHandler.enabled=true"})
+	class DisconnectWithErrorHandler {
+		@LocalServerPort
+		Integer port;
+
+		@Test
+		void test() throws Exception {
+			testDisconnectByHeartBeatTask(port);
+		}
+	}
+
+	@Nested
+	@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+	@DirtiesContext
+	@TestPropertySource(properties = {"errorHandler.enabled=false"})
+	class DisconnectWithoutErrorHandler {
+		@LocalServerPort
+		Integer port;
+
+		@Test
+		void test() throws Exception {
+			testDisconnectByHeartBeatTask(port);
+		}
 	}
 }
